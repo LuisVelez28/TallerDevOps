40d1c09588a121a6ee120bc26851cd7dea16cc17
LuisVelez28 <luvelezg@unal.edu.co>
2025-05-07 16:32:05 -0500

    Mostrar estudiantes ordenados en formato de tabla

--- estudiantes/registro.py
+++ estudiantes/registro.py
@@ -16,4 +16,32 @@ def cargar_estudiantes(archivo_csv):
                 print(f'Advertencia: Nota inválida para {nombre}')
     return estudiantes
 
-print(cargar_estudiantes('estudiantes.csv'))
+def mostrar_estudiantes_alfabeticamente(estudiantes):
+    copia = []
+    for e in estudiantes:
+        copia.append(e)
+    
+    for i in range(len(copia)):
+        for j in range(i + 1, len(copia)):
+            if copia[i]['nombre'] > copia[j]['nombre']:
+                temporal = copia[i]
+                copia[i] = copia[j]
+                copia[j] = temporal
+    
+    max_longitud = 6
+    for e in copia:
+        if len(e['nombre']) > max_longitud:
+            max_longitud = len(e['nombre'])
+    
+    linea = '-' * (max_longitud + 10)
+    
+    print("|NOMBRE".ljust(max_longitud + 1) + "| NOTA" + " |")
+    print(linea)
+    
+    for e in copia:
+        nombre = e['nombre'].ljust(max_longitud)
+        nota = str(e['nota']).rjust(4)
+        print("|" + nombre + "| " + nota + " |")
+
+estudiantes = cargar_estudiantes('estudiantes.csv')
+mostrar_estudiantes_alfabeticamente(estudiantes)
